9d084c299c0dc98a191980efdf8970f3eb167d03
Junaidchohane <junaidchohanedu@gmail.com>
2026-03-14 19:40:48 +0500

    Task no 04 store input numbers

---
+++ Netsole and NAVTTAC/NAVTTC Task_Projects/04_task.py
@@ -0,0 +1,11 @@
+# Take first number from the user
+num1 = int(input("Enter first number: "))
+
+# Take second number from the user
+num2 = int(input("Enter second number: "))
+
+# Calculate the sum of the two numbers
+sum_result = float(num1) + float(num2)
+
+# Display the result using an f-string
+print(f"The sum of {num1} and {num2} is {sum_result}")
